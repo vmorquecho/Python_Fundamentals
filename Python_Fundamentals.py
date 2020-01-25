b062d2de50cffdab0e9bd75c4644d219f68ee341
vmorquecho <morquechov@jdi.socom.mil>
2020-01-25 15:00:32 -0500

    updating with new information

--- Python_Fundamentals.py
+++ Python_Fundamentals.py
@@ -156,3 +156,89 @@ from words import (fetch_words, print_words)
 print_words(fetch_words())
 
 from words import * # imports everything from a module
+
+################### Documentation ###########################
+"""Retrive and print words from a URL. (module docstrings should be placed in beging of module)
+
+Usage:
+    python3.6 words.py <URL>
+    python3.6 words.py http://sixty-north.com/c/t.txt
+"""
+
+
+import sys
+from urllib.request import urlopen
+
+def fetch_words(url):
+    """ Fetch a list of words from a URL.
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns"
+        A list of stings
+    """
+    with urlopen(url) as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    return story_words
+# two line breaks betwen functions, PEP8
+
+def print_items(items):
+    for item in items:
+        print(item)
+
+
+def main(url):
+    """ need to accept as a command line arguement,
+    have to the 'sys.argv[]' attribute. Must 'import sys' module """
+    words = fetch_words(url)
+    print_items(words)
+
+if __name__ == '__main__':
+    main(sys.argv[1]) # The 0th arg is the module filename
+
+"""
+from words import * < this will import individual functions from 
+module and will allow to access individually.
+
+when using the docstring, you can call the docstring information
+with "help(fetch_words)" and it will print that functions respective
+docstring
+
+# Just in case you need to reload a module
+import importlib
+importlib.reload(module)
+
+"""
+
+chmod +x words.py # make file executable
+./words.py http://sixty-north.com/c/t.txt # execute
+"""
+Summary:
+Modules can be executed directly with
+    python module_name.py
+
+Brought into REPL or other moduels with
+    import module_name
+
+Named functions defined with the def keyword:
+   def function_name(arg1, argn)
+
+Return from functions usting "return" at end of function
+
+Use __name__ to determine how the module is being used
+
+If __name__ == "__main__" the module is being executed
+
+Command line arguments are accessible through sys.argv
+
+The script filename is in sys.argv[0]
+
+Docstrings provide help() in REPL
+
+"""
+
+############ "OBJECTS" module
--- words.py
+++ words.py
@@ -1,23 +1,48 @@
+#!/usr/bin/env python3.6
+"""Retrive and print words from a URL. (module docstrings should be placed in beging of module)
 
+Usage:
+    python3.6 words.py <URL>
+    python3.6 words.py http://sixty-north.com/c/t.txt
+"""
 
+import sys
 from urllib.request import urlopen
 
-def fetch_words():
-    with urlopen('http://sixty-north.com/c/t.txt') as story:
+def fetch_words(url):
+    """ Fetch a list of words from a URL.
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns"
+        A list of stings
+    """
+    with urlopen(url) as story:
         story_words = []
         for line in story:
             line_words = line.decode('utf-8').split()
             for word in line_words:
                 story_words.append(word)
     return story_words
+# two line breaks betwen functions, PEP8
 
-def print_words(story_words):
-    for word in story_words:
-        print(word)
+def print_items(items):
+    """Print items on per line.
+    
+    Args:
+        An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
 
-def main():
-    words = fetch_words()
-    print_words(words)
+
+def main(url):
+    """ need to accept as a command line arguement,
+    have to the 'sys.argv[]' attribute. Must 'import sys' module """
+    words = fetch_words(url)
+    print_items(words)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1]) # The 0
+
+""" python3.6 words.py http://sixty-north.com/c/t.txt """
