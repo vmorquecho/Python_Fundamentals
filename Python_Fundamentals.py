52c3030d9634a63f74193173bb0c086b289a84f7
vmorquecho <morquechov@jdi.socom.mil>
2020-01-25 13:11:03 -0500

    updating main file and created fetch.py

--- Python_Fundamentals.py
+++ Python_Fundamentals.py
@@ -125,3 +125,18 @@ import.reload(filename)
 
 # py 2.0
 reload(filename)
+
+###### 25 JAN 22020, "Modularity"
+
+from urllib.request import urlopen
+
+def fetch_words():
+    with urlopen('http://sixty-north.com/c/t.txt') as story:
+        story_words = []
+        for line in story:
+            line_words = line.decode('utf-8').split()
+            for word in line_words:
+                story_words.append(word)
+    
+    for word in story_words:
+        print(word)
